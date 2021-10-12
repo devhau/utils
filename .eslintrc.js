4b38ef89cb3cc3b4f07d0b7a58750a0902a3ff57
Nguyen Van Hau <nguyenvanhau.soft@gmail.com>
2021-10-12 22:27:44 +0700

    update

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        node: true
+    },
+    "extends": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": 5,
+        parser: 'babel-eslint'
+    },
+    "plugins": [],
+    "rules": {
+    }
+};
